62fd0daf4687338241d0352bd93ecd01e65c0b64
lantaris <edik.ponomarenko@mail.ru>
2020-04-03 14:19:30 +0300

    Fix mingw build

--- scripts/build-qxmpp-windows.sh
+++ scripts/build-qxmpp-windows.sh
@@ -2,9 +2,7 @@
 
 SCRIPT_FOLDER="$( cd "$( dirname "$0" )" && pwd )"
 
-${SCRIPT_FOLDER}/build-qxmpp.sh ${HOME}/Work/Qt/5.12.6/mingw32 " \
-        -DCMAKE_TOOLCHAIN_FILE=/home/roman/Work/demo-iotkit-qt/ext/virgil-iotkit/sdk/cmake/mingw32.toolchain.cmake  \
-        -DAUTOMOC_EXECUTABLE=/home/roman/Work/Qt/5.12.6/mingw32/bin/moc \
-        -DQT_MOC_EXECUTABLE=/home/roman/Work/Qt/5.12.6/mingw32/bin/moc
+${SCRIPT_FOLDER}/build-qxmpp.sh /opt/Qt/5.12.6/mingw32 " \
+        -DCMAKE_TOOLCHAIN_FILE=/usr/share/mingw/toolchain-mingw32.cmake  \
         -DCYGWIN=1\
     "
